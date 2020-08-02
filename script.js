ebc46583c979748648d4521ab85707ba500c3f36
kaimasaru <kainoa.accs@gmail.com>
2020-08-02 14:17:33 -0600

    second commit
    implemented simple fade transitions between pages being loaded. refactored page switching function to more efficiently and safely change between pages.

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const aboutBtn = document.querySelector("#about-btn");
 const shopBtn = document.querySelector("#shop-btn");
 const socialBtn = document.querySelector("#social-btn");
 
+const homePg = document.querySelector("#home-pg");
+const collectPg = document.querySelector("#collect-pg");
+const aboutPg = document.querySelector("#under-construction");
+const shopPg = document.querySelector("#under-construction");
+const socialPg = document.querySelector("#under-construction");
 
-let pageArr = document.getElementsByClassName("content");
+let pageArr = [homePg, collectPg, aboutPg, shopPg, socialPg];
 let btnArr = [homeBtn, collectBtn, aboutBtn, shopBtn, socialBtn];
 
-for(let i = 0; i < pageArr.length; i++) {
-    pageArr[i].classList.add("hidden");
-}
+let previousPage = 0;
+let activePage = 0;
 
-pageArr[0].classList.remove("hidden");
+let transitionTime = 250;
+
+pageArr[0].classList.add("show");
 
 homeBtn.classList.add("active");
 
@@ -31,19 +37,32 @@ homeBtn.classList.add("active");
 
 for(let i = 0; i < btnArr.length; i++) {
     btnArr[i].addEventListener("click", function() {
-        selectPage(i);
+        selectPage(i, activePage);
+        previousPage = activePage;
+        activePage = i;
     });
 }
 
-function selectPage(num) {
-    console.log("clicked");
-    btnArr.forEach(function(btn) {
-        btn.classList.remove("active");
-    });
-    for(let i = 0; i < pageArr.length; i++) {
-        pageArr[i].classList.add("hidden");
-    }
-    console.log(pageArr[num]);
-    btnArr[num].classList.add("active");
-    pageArr[num].classList.remove("hidden");
+function selectPage(newPage, oldPage) {
+    // newPage = index of the page/button that was clicked
+    // oldPage == which page/button we need to hide / make active
+    // need to log previous page.
+
+    // oldPage button -> remove active
+    // newPage button -> add active
+
+    btnArr[oldPage].classList.remove("active");
+    btnArr[newPage].classList.add("active");
+
+    // oldPage -> remove show and add hidden after transitionTime
+    // newPage -> after transitionTime remove hidden and add show
+
+    pageArr[oldPage].classList.remove("show");
+    console.log("begin transition");
+    setTimeout(function() {
+        console.log("swapping now");
+        pageArr[oldPage].classList.add("hidden");
+        pageArr[newPage].classList.remove("hidden");
+        pageArr[newPage].classList.add("show");
+    }, transitionTime);
 }
